20cbc235659f6ab2fa28d3036db6f16361f86de0
Iashenkovaias <141768835+Iashenkovaias@users.noreply.github.com>
2023-08-11 03:04:59 +0300

    There was found some mistake with while cycle by training other equations. This bug was fixed

--- equations/linear_equation.py
+++ equations/linear_equation.py
@@ -18,7 +18,7 @@ def task_16515():
 def task_16616():
   '''задача 16616 (и аналогичные) с портала https://kuzovkin.info/one_exercise_1/16616 Решите уравнение: (x−2)⋅(x−3)=(x+2)⋅(x−5)'''
   answer = [1.2345667]
-  while abs(int(answer[0]) - answer[0]) > 0.000001:
+  while True:
     x_int_1, x_int_2, = [random.randrange(1, 10) for i in range(1, 3)]
     int_1, int_2, int_3, int_4 = [random.randrange(-20, 20) for i in range(1, 5)]
     while int_1 * int_2 * int_3 * int_4 == 0:
@@ -27,4 +27,8 @@ def task_16616():
     eq = Eq((x_int_1*x + int_1) * (x_int_2 * x + int_2), (x_int_1 * x_int_2 * x + int_3) * (x + int_4))
     task = r'Решите уравнение: \(' +latex(eq)+'\)'
     answer = solve(eq, x)
+    if len(answer) > 0:
+      if abs(int(answer[0]) - answer[0]) < 0.000001:
+        break
+  return task, round(answer[0])
   return task, round(answer[0])
