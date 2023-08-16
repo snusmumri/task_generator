8033679d5b8e0f78464fa108a37d5ff33a1c74f3
Iashenkovaias <141768835+Iashenkovaias@users.noreply.github.com>
2023-08-17 00:12:25 +0300

    Extra values 'answer = [1.2345667]' ​​have been removed

--- equations/linear_equation.py
+++ equations/linear_equation.py
@@ -5,19 +5,20 @@ from sympy import *
 
 def task_16515():
   '''задача 16515 с портала https://kuzovkin.info/one_exercise_1/16515 Решите уравнение: 10⋅x2−(2⋅x−3)⋅(5⋅x−1)=31'''
-  answer = [1.2345667]
-  while abs(int(answer[0]) - answer[0]) > 0.000001:
+  while True:
     x_int_1, x_int_2, int_1, int_2  = np.random.randint(1, 30, size=4)
     eq_ans = random.randint(1, 300)
     x = symbols('x')
     eq = Eq(x_int_1 * x_int_2 * x**2 - (x_int_1 * x - int_1) * (x_int_2 * x - int_2), eq_ans)
     task = r'Решите уравнение: \(' +latex(eq)+'\)'
     answer = solve(eq, x)
+    if len(answer) > 0:
+      if abs(int(answer[0]) - answer[0]) < 0.000001:
+        break
   return task, round(answer[0])
 
 def task_16616():
   '''задача 16616 (и аналогичные) с портала https://kuzovkin.info/one_exercise_1/16616 Решите уравнение: (x−2)⋅(x−3)=(x+2)⋅(x−5)'''
-  answer = [1.2345667]
   while True:
     x_int_1, x_int_2, = [random.randrange(1, 10) for i in range(1, 3)]
     int_1, int_2, int_3, int_4 = [random.randrange(-20, 20) for i in range(1, 5)]
@@ -51,7 +52,6 @@ def task_16641():
 
 def task_16651():
   '''задача 16651 с портала https://kuzovkin.info/one_exercise_1/16651 Решите уравнение: (2⋅x+1)/5=1'''
-  answer = [1.2345667]
   while True:
     del_1, x_int = [random.randrange(1, 30) for i in range(1, 3)]
     int_1, int_2 = [random.randrange(-20, 20) for i in range(1, 3)]
@@ -69,7 +69,6 @@ def task_16651():
 
 def task_16653():
   '''задача 16653 с портала https://kuzovkin.info/one_exercise_1/16653 Решите уравнение: (11−3⋅x)/4=1/2'''
-  answer = [1.2345667]
   while True:
     n = random.randint(1, 20)
     m = random.randint(n+1, n+30)
@@ -90,7 +89,6 @@ def task_16653():
 
 def task_16654():
   '''задача 16654 с портала https://kuzovkin.info/one_exercise_1/16654 Решите уравнение: (3⋅x+7)/5=(6⋅x+4)/5'''
-  answer = [1.2345667]
   while True:
     x_int_1, x_int_2 = [random.randrange(1, 30) for i in range(1, 3)]
     int_1, int_2 = [random.randrange(-20, 20) for i in range(1, 3)]
@@ -109,7 +107,6 @@ def task_16654():
 
 def task_16655():
   '''задача 16655, 16657 с портала https://kuzovkin.info/one_exercise_1/16655 Решите уравнение: 3⋅x−(2⋅x−1)/5=(3⋅x−19)/5'''
-  answer = [1.2345667]
   while True:
     x_int_1, x_int_2, x_int_3 = [random.randrange(1, 30) for i in range(1, 4)]
     int_1, int_2 = [random.randrange(-20, 20) for i in range(1, 3)]
@@ -128,7 +125,6 @@ def task_16655():
 
 def task_16656():
   '''задача 16656, 16658 с портала https://kuzovkin.info/one_exercise_1/16656 Решите уравнение: (8⋅x−3)/7−(3⋅x+1)/10=2'''
-  answer = [1.2345667]
   while True:
     x_int_1, x_int_2, del_1, del_2 = [random.randrange(1, 30) for i in range(1, 5)]
     int_1, int_2, int_3 = [random.randrange(-20, 20) for i in range(1, 4)]
