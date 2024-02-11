e960d3bb0c9a3a058bc1074fc65e1b34b8b7ef0a
JustBodya <bagdancanturk@gmail.com>
2024-02-11 19:21:01 +0300

    upgrate of task_for_procent and context.json

--- text_tasks/task_for_procent.py
+++ text_tasks/task_for_procent.py
@@ -56,7 +56,7 @@ def task_930():
 def task_1039():
     '''Генерация аналогичных задач № 1039 с портала https://kuzovkin.info/one_exercise_1/1039
     В растворе массой 280 г содержится 56 г соли. Какова концентрация этого раствора?'''
-    product = random.sample(generate_context('task_generator/text_tasks/context.json', 'products'), 1)
+    product = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'products'))
     while True:
         number, number1 = np.random.randint(1, 1000, size=2)
         if number <= number1:
@@ -73,7 +73,7 @@ def task_1047():
     '''Генерация аналогичных задач № 1047 с портала https://kuzovkin.info/one_exercise_1/1047
     В семенах подсолнечника нового сорта содержится 49,5% масла.
     Сколько килограммов таких семян надо взять, чтобы в них содержалось 29,7 кг масла?'''
-    oil = random.sample(generate_context('task_generator/text_tasks/context.json', 'oils'), 1)
+    oil = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'oils'))
     while True:
         number, number1 = np.random.randint(1, 100, size=2)
         if number <= number1:
@@ -104,7 +104,7 @@ def task_621():
     '''Генерация аналогичных задач № 621 с портала https://kuzovkin.info/one_exercise_1/621
     Проезд в маршрутке от станции метро Университет до ГЗ МГУ имени М. В. Ломоносова подорожал с 20 рублей до 25 рублей.
     На сколько процентов повысилась цена проезда?'''
-    transport = random.sample(generate_context('task_generator/text_tasks/context.json', 'transports'), 1)
+    transport = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'transports'))
     while True:
         number, number1 = np.random.randint(10, 100, size=2)
         if number >= number1:
@@ -311,7 +311,7 @@ def task_833():
     '''Генерация аналогичных задач № 833 с портала https://kuzovkin.info/one_exercise_1/833
     Мальчик Гриша прочитал в первый день 30% всей книги, во второй − 40% оставшейся части, а в третий − оставшиеся 105 страниц.
     Сколько всего страниц было в книге?'''
-    book = random.sample(generate_context('task_generator/text_tasks/context.json', 'books'), 1)
+    book = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'books'))
     name = np.random.choice(['Гриша', 'Коля', 'Ваня'])
     while True:
         number, number1, number2 = np.random.randint(1, 50), np.random.randint(1, 50), np.random.randint(100, 200)
@@ -449,7 +449,7 @@ def task_4468():
     '''Генерация аналогичных задач № 4468 с портала https://kuzovkin.info/one_exercise_1/4468
     Концентрация серной кислоты в растворе составляет 22%.
     Сколько чистой серной кислоты и сколько воды содержится в 150 г раствора?'''
-    acid = random.sample(generate_context('task_generator/text_tasks/context.json', 'acids'), 1)
+    acid = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'acids'))
     while True:
         number, number1 = np.random.randint(1, 95), np.random.randint(10, 500)
         result, result1 = (number / 100) * number1, ((100 - number) / 100) * number1
@@ -463,7 +463,7 @@ def task_4468():
 def task_4471():
     '''Генерация аналогичных задач № 4471 с портала https://kuzovkin.info/one_exercise_1/4471
     Сколько воды надо добавить к 30 г соли, чтобы получить пятипроцентный раствор соли?'''
-    product = random.sample(generate_context('task_generator/text_tasks/context.json', 'products'), 1)
+    product = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'products'))
     percent = np.random.choice(['пятипроцентный', 'четырехпроцентный', 'десятипроцентный'])
     while True:
         number = np.random.randint(1, 100)
@@ -485,7 +485,7 @@ def task_4471():
 def task_4474():
     '''Генерация аналогичных задач № 4474 с портала https://kuzovkin.info/one_exercise_1/4474
     Сколько соли надо добавить к 190 г воды, чтобы получить пятипроцентный раствор соли?'''
-    product = random.sample(generate_context('task_generator/text_tasks/context.json', 'products'), 1)
+    product = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'products'))
     percent = np.random.choice(['пятипроцентный', 'четырехпроцентный', 'десятипроцентный'])
     while True:
         number = np.random.randint(100, 500)
@@ -501,4 +501,332 @@ def task_4474():
             answer = int(result)
             break
     task = f'Сколько {choosing_declension_form(product)} надо добавить к {number} г воды, чтобы получить {percent} раствор {choosing_declension_form(product)}?'
+    return task, answer
+
+
+
+
+
+def task_4488():
+    '''Генерация аналогичных задач № 4488 с портала https://kuzovkin.info/one_exercise_1/4488
+    Стоимость покупки с учётом четырёхпроцентной скидки по дисконтной карте составила 1152 рубля.
+    Сколько рублей пришлось бы заплатить за покупку при отсутствии дисконтной карты?'''
+    percent = np.random.choice(['пятипроцентной', 'четырёхпроцентной', 'десятипроцентной'])
+    value = np.random.choice(['товар', 'покупка', 'продукт'])
+    while True:
+        number = np.random.randint(1000, 100000)
+
+        if percent == 'пятипроцентной':
+            result = number / (95 / 100)
+        elif percent == 'четырёхпроцентной':
+            result = number / (96 / 100)
+        else:
+            result = number / (90 / 100)
+
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'Стоимость {choosing_declension_form(value)} с учётом {percent} скидки по дисконтной карте составила {number} рубля. Сколько рублей пришлось бы заплатить за {choosing_declension_form(value, "accs")} при отсутствии дисконтной карты?'
+    return task, answer
+
+
+def task_4497():
+    '''Генерация аналогичных задач № 4497 с портала https://kuzovkin.info/one_exercise_1/4497
+    В первом квартале доля рынка, занимаемая товарами отечественных производителей, увеличилась с 20% до 25% процентов, а во втором − с 25% до 30%.
+    На сколько процентов увеличилась отечественная доля рынка во 2 квартале?'''
+    product = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'goods'))
+    while True:
+        number, number1, number2 = np.random.randint(1, 95, size=3)
+        if number >= number1 or number1 >= number2:
+          continue
+        result = number2 - number1
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'В первом квартале доля рынка, занимаемая {choosing_declension_form(product, "ablt")} товарами, увеличилась с {number}% до {number1}% процентов, а во втором − с {number1}% до {number2}%. На сколько процентов увеличилась доля рынка во 2 квартале?'
+    return task, answer
+
+
+def task_4498():
+    '''Генерация аналогичных задач № 4498 с портала https://kuzovkin.info/one_exercise_1/4498
+    В первом квартале доля рынка, занимаемая товарами отечественных производителей, увеличилась с 20% до 25% процентов, а во втором − с 25% до 30%.
+    В каком квартале увеличение было более значительным(в ответ написать разность процентов между первым и вторым)?'''
+    product = np.random.choice(generate_context('task_generator/text_tasks/context.json', 'goods'))
+    while True:
+        number, number1, number2 = np.random.randint(1, 95, size=3)
+        if number >= number1 or number1 >= number2:
+          continue
+        result = (number1 - number) - (number2 - number1)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'В первом квартале доля рынка, занимаемая {choosing_declension_form(product, "ablt")} товарами, увеличилась с {number}% до {number1}% процентов, а во втором − с {number1}% до {number2}%. В каком квартале увеличение было более значительным(в ответ написать разность процентов между первым и вторым)?'
+    return task, answer
+
+
+def task_4501():
+    '''Генерация аналогичных задач № 4501 с портала https://kuzovkin.info/one_exercise_1/4501
+    Кофе при обжаривании теряет 12,5% своего веса.
+    Сколько килограммов свежего кофе нужно взять, чтобы получить 42 кг жареного?'''
+    product = np.random.choice(['банан', 'виноград', 'абрикос'])
+    while True:
+        percent = round(np.random.uniform(60, 85), 2)
+        number = np.random.randint(1, 100)
+        if percent * 10 % 10 == 0:
+          continue
+        result = number / ((100 - percent) / 100)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'{product.capitalize()} при сушки теряет {percent}% своего веса. Сколько килограммов {choosing_declension_form(product)} нужно взять, чтобы получить {number} кг сушеного?'
+    return task, answer
+
+
+def task_4510():
+    '''Генерация аналогичных задач № 4510 с портала https://kuzovkin.info/one_exercise_1/4510
+    Туристы проехали 50% пути на поезде и 40% пути на автобусе. Какую часть пути осталось пройти туристам?'''
+    value = np.random.choice(['люди', 'туристы', 'пассажиры'])
+    while True:
+        number, number1 = np.random.randint(10, 100, size=2)
+        if number + number1 >= 100:
+          continue
+        result = 100 - (number + number1)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'{value.capitalize()} проехали {number}% пути на поезде и {number1}% пути на автобусе. Какую часть пути осталось пройти {choosing_declension_form(value, "datv")}?'
+    return task, answer
+
+
+def task_4512():
+    '''Генерация аналогичных задач № 4512 с портала https://kuzovkin.info/one_exercise_1/4512
+    Сплав меди, цинка и олова содержит 20% меди и 45% цинка. Сколько в этом сплаве олова?'''
+    metal, metal_1, metal_2 = random.sample(generate_context('task_generator/text_tasks/context.json', 'metals'), 3)
+    while True:
+        number, number1 = np.random.randint(10, 100, size=2)
+        if number + number1 >= 100:
+          continue
+        result = 100 - (number + number1)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'Сплав {choosing_declension_form(metal)}, {choosing_declension_form(metal_1)} и {choosing_declension_form(metal_2)} содержит {number}% {choosing_declension_form(metal)} и {number1}% {choosing_declension_form(metal_1)}. Сколько в этом сплаве {choosing_declension_form(metal_2)}?'
+    return task, answer
+
+
+def task_4517():
+    '''Генерация аналогичных задач № 4517 с портала https://kuzovkin.info/one_exercise_1/4517
+    Основным материалом при изготовлении самолётов является сплав дюралюминий (другие названия − дуралюмин, дюраль).
+    Дюралюминий − самый прочный сплав среди сплавов на основе алюминия.
+    Кроме алюминия, дюралюминий содержит медь (1,4%), магний (0,4%), марганец (1%), кремний (0,5%), цинк (7%), железо (1,8%) и титан (0,35%).
+    Сколько в таком сплаве процентов алюминия?'''
+    metal, metal_1, metal_2, metal_3, metal_4, metal_5, metal_6, metal_7 = random.sample(generate_context('task_generator/text_tasks/context.json', 'metals'), 8)
+    while True:
+        number, number1, number2, number3, number4, number5, number6 = np.round(np.random.uniform(0, 10, size=7), 2)
+        numbers_list = [number, number1, number2, number3, number4, number5 ,number6]
+        for i in numbers_list:
+          if i * 10 % 10 == 0:
+            continue
+        result = 100 - (number + number1 + number2 + number3 + number4 + number5 + number6)
+        if result * 10 % 10 != 0 and len(str(result)) <= 5:
+            answer = result
+            break
+    task = f'Сплав, кроме {choosing_declension_form(metal)}, содержит {choosing_declension_form(metal_1, "accs")} ({number}%), {choosing_declension_form(metal_2, "accs")} ({number1}%), {choosing_declension_form(metal_3, "accs")} ({number2}%), {choosing_declension_form(metal_4, "accs")} ({number3}%), {choosing_declension_form(metal_5, "accs")} ({number4}%), {choosing_declension_form(metal_6, "accs")} ({number5}%) и {choosing_declension_form(metal_7, "accs")} ({number6}%). Сколько в таком сплаве процентов {choosing_declension_form(metal)}?'
+    return task, answer
+
+
+def task_4521():
+    '''Генерация аналогичных задач № 4521 с портала https://kuzovkin.info/one_exercise_1/4521
+    Который теперь час, если остающаяся часть суток равна 60% протекшей части?'''
+    while True:
+        number = np.random.randint(1, 100)
+        total = 86400 / (1 + number / 100)
+        if total * 10 % 10 != 0:
+          continue
+        hours = int(total / 3600)
+        minutes = int(total / 60) - (hours * 60)
+        seconds = int(total % 60)
+
+        if hours <= 9:
+            hours = f'0{hours}'
+        if minutes <= 9:
+            minutes = f'0{minutes}'
+        if seconds <= 9:
+            seconds = f'0{seconds}'
+
+        answer = f'{hours}:{minutes}:{seconds}'
+        break
+    task = f'Который теперь час, если остающаяся часть суток равна {number}% протекшей части?'
+    return task, answer
+
+
+def task_4526():
+    '''Генерация аналогичных задач № 4526 с портала https://kuzovkin.info/one_exercise_1/4526
+    Найдите: 34,1% от 1010'''
+    while True:
+        number, number1 = round(np.random.uniform(1, 99), 2), np.random.randint(1000, 10000)
+        if number * 10 % 10 == 0:
+          continue
+        result = number1 * (number / 100)
+        if len(str(result)) <= 5 and result * 10 % 10 != 0:
+            answer = result
+            break
+    task = f'Найдите: {number}% от {number1}'
+    return task, answer
+
+
+def task_4535():
+    '''Генерация аналогичных задач № 4535 с портала https://kuzovkin.info/one_exercise_1/4535
+    Найдите: 0,4% от 55'''
+    while True:
+        number, number1 = round(np.random.uniform(0, 1), 2), np.random.randint(10, 100)
+        if number * 10 % 10 == 0:
+          continue
+        result = number1 * (number / 100)
+        if len(str(result)) <= 5 and result * 10 % 10 != 0:
+            answer = result
+            break
+    task = f'Найдите: {number}% от {number1}'
+    return task, answer
+
+
+def task_4542():
+    '''Генерация аналогичных задач № 4542 с портала https://kuzovkin.info/one_exercise_1/4542
+    Найдите: 13,4% от 180'''
+    while True:
+        number, number1 = round(np.random.uniform(10, 99), 2), np.random.randint(100, 1000)
+        if number * 10 % 10 == 0:
+          continue
+        result = number1 * (number / 100)
+        if len(str(result)) <= 5 and result * 10 % 10 != 0:
+            answer = result
+            break
+    task = f'Найдите: {number}% от {number1}'
+    return task, answer
+
+
+def task_4543():
+    '''Генерация аналогичных задач № 4543 с портала https://kuzovkin.info/one_exercise_1/4543
+    Найдите: 0,5% от 12'''
+    while True:
+        number, number1 = round(np.random.uniform(0, 1), 2), np.random.randint(1, 100)
+        if number * 10 % 10 == 0:
+          continue
+        result = number1 * (number / 100)
+        if len(str(result)) <= 5 and result * 10 % 10 != 0:
+            answer = result
+            break
+    task = f'Найдите: {number}% от {number1}'
+    return task, answer
+
+
+def task_4547():
+    '''Генерация аналогичных задач № 4547 с портала https://kuzovkin.info/one_exercise_1/4547
+    Найдите: 9,75% от 540'''
+    while True:
+        number, number1 = round(np.random.uniform(10, 99), 2), np.random.randint(100, 1000)
+        if number * 10 % 10 == 0:
+          continue
+        result = number1 * (number / 100)
+        if len(str(result)) <= 5 and result * 10 % 10 != 0:
+            answer = result
+            break
+    task = f'Найдите: {number}% от {number1}'
+    return task, answer
+
+
+def task_4551():
+    '''Генерация аналогичных задач № 4551 с портала https://kuzovkin.info/one_exercise_1/4551
+    Найдите: 7,45% от 56,2'''
+    while True:
+        number, number1 = np.round(np.random.uniform(1, 99, size=2), 2)
+        if number * 10 % 10 == 0 or number1 * 10 % 10 == 0:
+          continue
+        result = number1 * (number / 100)
+        if len(str(result)) <= 5 and result * 10 % 10 != 0:
+            answer = result
+            break
+    task = f'Найдите: {number}% от {number1}'
+    return task, answer
+
+
+def task_4556():
+    '''Генерация аналогичных задач № 4556 с портала https://kuzovkin.info/one_exercise_1/4556
+    Найдите целое, если 15% равны 45'''
+    while True:
+        number, number1 = np.random.randint(10, 99, size=2)
+        result = number1 / (number / 100)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'Найдите целое, если {number}% равны {number1}'
+    return task, answer
+
+
+def task_4557():
+    '''Генерация аналогичных задач № 4557 с портала https://kuzovkin.info/one_exercise_1/4557
+    Найдите целое, если 19% равны 57'''
+    while True:
+        number, number1 = np.random.randint(10, 99, size=2)
+        result = number1 / (number / 100)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'Найдите целое, если {number}% равны {number1}'
+    return task, answer
+
+
+def task_4559():
+    '''Генерация аналогичных задач № 4559 с портала https://kuzovkin.info/one_exercise_1/4559
+    Найдите целое, если 35% равны 105'''
+    while True:
+        number, number1 = np.random.randint(10, 99), np.random.randint(100, 1000)
+        result = number1 / (number / 100)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'Найдите целое, если {number}% равны {number1}'
+    return task, answer
+
+
+def task_4562():
+    '''Генерация аналогичных задач № 4562 с портала https://kuzovkin.info/one_exercise_1/4562
+    Найдите целое, если 120% равны 360'''
+    while True:
+        number, number1 = np.random.randint(100, 1000, size=2)
+        result = number1 / (number / 100)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'Найдите целое, если {number}% равны {number1}'
+    return task, answer
+
+
+def task_4564():
+    '''Генерация аналогичных задач № 4564 с портала https://kuzovkin.info/one_exercise_1/4564
+    Найдите целое, если 5% равны 2,5'''
+    while True:
+        number, number1 = np.random.randint(1, 100), round(np.random.uniform(1, 10), 2)
+        if number1 * 10 % 10 == 0:
+          continue
+        result = number1 / (number / 100)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'Найдите целое, если {number}% равны {number1}'
+    return task, answer
+
+
+def task_4565():
+    '''Генерация аналогичных задач № 4565 с портала https://kuzovkin.info/one_exercise_1/4565
+    Найдите целое, если 7% равны 0,77'''
+    while True:
+        number, number1 = np.random.randint(1, 100), round(np.random.uniform(0, 1), 2)
+        if number1 * 10 % 10 == 0:
+          continue
+        result = number1 / (number / 100)
+        if result * 10 % 10 == 0:
+            answer = int(result)
+            break
+    task = f'Найдите целое, если {number}% равны {number1}'
     return task, answer
